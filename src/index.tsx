ce6ff045b302095896675477b3c6cd20091de55e
svakel <sari.vakela@gmail.com>
2019-03-24 19:53:07 +0200

    webpack, react and typescript added

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+console.log('kukkuu');
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render (
+  <App color="Blue" />,
+  document.getElementById("root")
+);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+
+
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
+    output: {
+      filename: "bundle.js",
+      // path: path.resolve(__dirname + "/dist"),
+      path: resolve(__dirname, 'dist'),
+      // publicPath: "/public/",
+  },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "inline-source-map",
+    mode: 'development',
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+    },
+
     module: {
         rules: [
           {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: ['babel-loader']
-          }
-        ]
+          },
+          
+          {  
+            test: /\.tsx?$/,
+            loader: "ts-loader"
+          },
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+          
+      ]
     },
-    resolve: {
-    extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-      path: __dirname + 'build',
-      publicPath: '/',
-      filename: 'bundle.js'
-      },
+
+    // When importing a module whose path matches one of the following, just
+          // assume a corresponding global variable exists and use that instead.
+          // This is important because it allows us to avoid bundling all of our
+          // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // },
+
+    // plugins: [
+    //   new HtmlWebpackPlugin({
+    //     template: './src/index.html'
+    //   })
+    // ],
+ 
     devServer: {
-      contentBase: './build'
-      }
+      // contentBase: './build',
+      // port: 3040,
+    }
   };
